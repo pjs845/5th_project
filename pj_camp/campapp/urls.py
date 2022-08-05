16c80ed75be0415a7a53b8f2aaa99dc7f1ede6ba
pjs <awlstjrdla@naver.com>
2022-08-05 16:50:52 +0900

    modified

--- pj_camp/campapp/urls.py
+++ pj_camp/campapp/urls.py
@@ -5,7 +5,12 @@ urlpatterns = [
     path('', views.main_page, name='main'),
     path('paging/', views.paging, name='paging'),
     path('signup/', views.signup, name='sugnup'),
+    path('signup/signup_ok', views.signup_ok, name = 'signup_ok'),
     path('board/', views.board_page, name='board'),
     path('page2/', views.ad_page, name='page2'),
+    path('login/', views.login, name='login'),
+    path('login/login_ok/', views.login_ok, name='login_ok'),
+    path('logout', views.logout, name='logout'),
     path('notice/', views.notice, name='notice'),
+    path('notice/content/<int:id>', views.content, name='content'),
 ]
