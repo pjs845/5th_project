3a7be7c70576f7be201c85615b98f24b3981a2be
Bomin Kim <bomin3256@gmail.com>
2022-08-05 10:09:32 +0900

    login
    added files n modified login.html.url.views..

---
+++ pj_camp/pj_camp/campapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.main_page, name='main'),
+    path('paging/', views.paging, name='paging'),
+    path('signup/', views.signup, name='signup'),
+    path('signup/signup_ok', views.signup_ok, name = 'signup_ok'),
+    path('board/', views.board_page, name='board'),
+    path('page2/', views.ad_page, name='page2'),
+    path('login/', views.login, name='login'),
+    path('login/login_ok/', views.login_ok, name='login_ok'),
+    
+]
